81ad13397e0f188e009a2dd44c1c45fecff2f6c4
Mahesh Karampuri <maheshkarampuri4@gmail.com>
2025-12-25 18:27:01 +0530

    feat: add theater-service module with Eureka config

---
+++ theater-service/src/test/java/com/preshow/theater/TheaterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preshow.theater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
