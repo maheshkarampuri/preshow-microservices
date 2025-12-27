863aee7df60fee7c711332397127c8fd112e91ff
Mahesh Karampuri <maheshkarampuri4@gmail.com>
2025-12-27 12:56:15 +0530

    feat: implement seat-service

---
+++ seat-service/src/main/java/com/preshow/seat/SeatServiceApplication.java
@@ -0,0 +1,14 @@
+package com.preshow.seat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatServiceApplication.class, args);
+        System.out.println("Successfully Executed Seat Service");
+    }
+
+}
