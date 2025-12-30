0ad1837eb87eaa65cfe8656d15727b0b4fd737f9
Mahesh Karampuri <maheshkarampuri4@gmail.com>
2025-12-30 12:51:17 +0530

    Add show-query-service module

---
+++ show-query-service/src/main/java/com/preshow/showquery/ShowQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.preshow.showquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowQueryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowQueryServiceApplication.class, args);
+    }
+
+}
---
+++ show-query-service/src/test/java/com/preshow/showquery/ShowQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preshow.showquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
